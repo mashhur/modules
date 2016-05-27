117777f0f179b73b9d165ae4b802892baf26f869
mashhur <mashhur.sattorov@gmail.com>
2016-05-27 15:35:36 +0900

    nodejs AES256
    nodejs AES256 module

---
+++ crypto/nodejseas256/app.js
@@ -0,0 +1,30 @@
+var AESCrypt = {};
+
+AESCrypt.decrypt = function(cryptkey, iv, encryptdata) {
+    encryptdata = new Buffer(encryptdata, 'base64').toString('binary');
+
+    var decipher = crypto.createDecipheriv('aes-256-cbc', cryptkey, iv),
+        decoded = decipher.update(encryptdata, 'binary', 'utf8');
+
+    decoded += decipher.final('utf8');
+    return decoded;
+}
+
+AESCrypt.encrypt = function(cryptkey, iv, cleardata) {
+    var encipher = crypto.createCipheriv('aes-128-cbc', cryptkey, iv),
+        encryptdata = encipher.update(cleardata, 'utf8', 'binary');
+
+    encryptdata += encipher.final('binary');
+    encode_encryptdata = new Buffer(encryptdata, 'binary').toString('base64');
+    return encode_encryptdata;
+}
+
+var cryptkey   = crypto.createHash('sha256').update('Nixnogen').digest(),
+    iv         = 'Aa61UX12zYR4z8SX',	// key
+    buf        = "mashhur", // 32 chars, text
+    enc        = AESCrypt.encrypt(cryptkey, iv, buf);
+var dec        = AESCrypt.decrypt(cryptkey, iv, enc);
+
+console.warn("encrypt length: ", enc.length);
+console.warn("encrypt in Base64:", enc);
+console.warn("decrypt all: " + dec);
